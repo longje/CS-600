c01160e65f117ae2629ad3f05d8497435980a6fe
Jason <jalong@jalong-laptop>
2012-03-08 14:57:02 -0500

    Set solution table to hold palindrome character sequence

--- Palindrome/src/Program.java
+++ Palindrome/src/Program.java
@@ -1,15 +1,13 @@
 public class Program {
 
-    //static char[] y = new char[]{ 's', 'p', 'a', 'n', 'k', 'i', 'n', 'g' };
-    //static char[] y = new char[]{ 'c', 'h', 'a', 'r', 'a', 'c', 't', 'e', 'r', 's', 'a' };
+    //Test on MAHDYNAMICPROGRAMZLETMESHOWYOUTHEM
+    //Solution is MHYMRORMYHM
     
-    //Failed on MAHDYNAMICPROGRAMZLETMESHOWYOUTHEM
-    // Real solution is MHYMRORMYHM
-    //Only came up with HARORAH
-    
-    //static char[] y = "MAHDYNAMICPROGRAMZLETMESHOWYOUTHEM".toCharArray();
-    static char[] y = "character".toCharArray();
+    static char[] y = "MAHDYNAMICPROGRAMZLETMESHOWYOUTHEM".toCharArray();
+    //static char[] y = "character".toCharArray();
     static int[][] repeat = new int[y.length][y.length];
+    static String[][] solution = new String[y.length][y.length];
+    
     static int[] track = new int[y.length];
     static String[] subseqs = new String[y.length];
     static String res = "";
@@ -17,15 +15,22 @@ public class Program {
         
         for (int i = 0; i < y.length ; i++)
         {
-            repeat[i][i] = 1;
+            //repeat[i][i] = 1;
+        	solution[i][i] = y[i] + "";
         }
         
         for (int i = 0; i < y.length - 1 ; i++)
         {
         	if (y[i] == y[i + 1])
-        		repeat[i][i + 1] = 2;
+        	{
+        		//repeat[i][i + 1] = 2;
+        		solution[i][i+1] = solution[i][i] + y[i]; 
+        	}
         	else
-        		repeat[i][i+1] = 1;
+        	{
+        		//repeat[i][i+1] = 1;
+        		solution[i][i+1] = y[i] + "";
+        	}
         }
         
         for (int i = y.length - 1; i >= 0; i--)
@@ -36,79 +41,25 @@ public class Program {
         	{
         		System.out.print(y[j]);
         		if (y[i] == y[j]) {
-        			repeat[i][j] = repeat[i + 1][j - 1] + 2;
-        			temp += y[j];
-					temp = y[j] + temp;
-					subseqs[i] += temp + subseqs[j - 1];
+        			//repeat[i][j] = repeat[i + 1][j - 1] + 2;
+        			solution[i][j] = y[i] + solution[i + 1][j - 1] + y[i];
         		}
         		else
         		{
-        			repeat[i][j] = (repeat[i + 1][j] > repeat[i][j-1])
-                    					? repeat[i+1][j]
-                    					: repeat[i][j-1];
-                    subseqs[i] = y[i] + "";
+        			//repeat[i][j] = (repeat[i + 1][j] > repeat[i][j-1])
+                    	//				? repeat[i+1][j]
+                    		//			: repeat[i][j-1];
+                    solution[i][j] = (solution[i + 1][j].length() > solution[i][j-1].length())
+										? solution[i+1][j]
+										: solution[i][j-1];
+                    
         		}
         	}
-        	System.out.println();
-        	System.out.println(subseqs[i]);
-        	if (temp.length() > res.length())
-				res = temp;
         }
         
-        /*
-        for (int i = repeat[0].length - 2; i > 0; i--)
-        	if (repeat[0][i] > repeat[0][i-1])
-        		res += y[i];
-        	else if (repeat[0][i] > repeat[0][i+1])
-        	{
-        		String temp = "";
-        		for(char x : res.toCharArray())
-        		{
-        			temp = x + temp;
-        		}
-        		res += y[i];
-        		res += temp;
-        	}
-*/
-        //int runningVal = 1;
-        
-        /*
-        for (int i = 1; i < y.length - 1; i++)
-        {
-            String palindrome = y[i] + "";
-            
-            for (int j = 1; j < y.length - i; j++)
-            {
-                int s = j+i;
-                repeat[j][s] = computecost(repeat, y, j, s);
-                repeat[s][j] = computecost(repeat, y, j, s);
-                
-            }
-            //if (palindrome.length() > palindrome.length())
-                //result = palindrome;
-        }
-        */
         System.out.println("Longest palindrome subsequence is: " + res);
-        printArray(repeat);
+        printArray(solution);
 
-    }
-    
-    private static int computecost(int[][] result, char[] seq, int i, int j)
-    {
-        if (i == j)
-            return result[i][j];
-        else if (seq[i] == seq[j])
-        {
-        	res += seq[i];
-            if (i + 1 < j - 1)
-                return result[i+1][j-1] + 2;
-            else
-                return 2;
-        }
-        else
-            return (result[i+1][j] > result[i][j-1])
-                        ? result[i+1][j]
-                        : result[i][j-1];
     }
     
     private static void printSingleArray(int[] array)
@@ -119,25 +70,25 @@ public class Program {
         }
     }
     
-    private static void printArray(int[][] array)
+    private static void printArray(String[][] array)
     {
         Boolean printHead = false;
-        for (int i = 0; i < repeat.length; i++)
+        for (int i = 0; i < array.length; i++)
         {
             
             if (!printHead) {
                 System.out.print("  ");
                 for (int x = 0; x < y.length; x++)
-                    System.out.print(y[x] + " ");
+                    System.out.print(y[x] + "\t");
                 System.out.println();
                 printHead = true;
             }
             
             System.out.print(y[i] + " ");
             
-            for (int j = 0; j < repeat[i].length; j++)
+            for (int j = 0; j < array[i].length; j++)
             {
-                System.out.print(repeat[i][j] + " ");
+                System.out.print(array[i][j] + "\t");
             }
             System.out.println();
         }
@@ -145,90 +96,3 @@ public class Program {
     }
 
 }
-
-
-
-/*
-public class Program {
-
-	//static char[] y = new char[]{ 's', 'p', 'a', 'n', 'k', 'i', 'n', 'g' };
-	//static char[] y = new char[]{ 'c', 'h', 'a', 'r', 'a', 'c', 't', 'e', 'r', 's', 'a' };
-	
-	//Failed on MAHDYNAMICPROGRAMZLETMESHOWYOUTHEM 
-	// Real solution is MHYMRORMYHM
-	//Only came up with HARORAH
-	
-	static char[] y = "MAHDYNAMICPROGRAMZLETMESHOWYOUTHEM".toCharArray();
-	static int[][] repeat = new int[8][8];
-	static int[] track = new int[y.length];
-	
-	public static void main(String[] args) {
-		
-		
-		for (int i = 0; i < y.length; i++)
-		{
-			for (int j = i + 1; j < y.length; j++)
-			{
-				if (Character.valueOf(y[i]).compareTo(y[j]) == 0)
-				{
-					track[j] = i + 1;
-				}
-			}
-		}
-		
-		String result = "";
-		
-		for (int i = 0; i < track.length; i++)
-		{
-			
-			String temp = y[i] + "";
-			int lastAdd = 10000000;
-			
-			for (int j = i + 1; j < track.length; j++)
-			{
-				if (track[j] == 0)
-					continue;
-				
-				if (track[j] - 1 < i && track[j] < lastAdd)
-				{
-					lastAdd = track[j];
-					temp += y[track[j] - 1];
-					temp = y[track[j] - 1] + temp;
-				}
-			}
-			
-			if (temp.length() > result.length())
-				result = temp;
-			
-			System.out.println("Longest palindrome subsequence is: " + result);
-			
-		}
-		System.out.println("Longest palindrome subsequence is: " + result);
-		printSingleArray(track);
-
-	}
-	
-	private static void printSingleArray(int[] array)
-	{
-		for(int x: track)
-		{
-			System.out.print(x + " ");
-		}
-	}
-	
-	private static void printArray(int[][] array)
-	{
-		for (int i = 0; i < repeat.length; i++)
-		{
-			//System.out.print(repeat[i] + " ");
-			for (int j = 0; j < repeat[i].length; j++)
-			{
-				System.out.print(repeat[i][j] + " ");
-			}
-			System.out.println();
-		}
-		
-	}
-
-}
-*/
