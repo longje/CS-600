3cea7eb569f88a5aaf9b984ecf8f7a207961547d
Jason <jalong@jalong-laptop>
2012-03-12 21:28:21 -0400

    Updated LCS with recursive and memoize versions.

---
+++ LCS/src/Results.java
@@ -0,0 +1,19 @@
+
+public class Results {
+	public int SeqA;
+	public int SeqB;
+	
+	public long dyntime;
+	public long rectime;
+	public long memotime;
+	
+	public String dynlcs;
+	public String reclcs;
+	public String memolcs;
+	
+	public Results(int a, int b)
+	{
+		SeqA = a;
+		SeqB = b;
+	}
+}
